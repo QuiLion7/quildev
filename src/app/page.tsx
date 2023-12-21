95f7af3e5a9cb09f2d43e3a30b4a407121a421d9
QuiLion7 <quilbrub@gmail.com>
2023-12-20 22:09:52 -0300

    feat: modify homepage layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Home() {
   return (
-    <div>
-      <ModeToggle />
+    <div className="relative container flex justify-center items-center mt-1">
+      <div className="absolute top-10 right-10">
+        <ModeToggle />
+      </div>
       <p>Iniciando Projeto</p>
     </div>
   );
