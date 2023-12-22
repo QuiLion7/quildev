45732bfc16bb27190746420452c910f097a2a9cf
QuiLion7 <quilbrub@gmail.com>
2023-12-22 03:56:59 -0300

    feat: create data file for header links

---
+++ src/app/utils/data.ts
@@ -0,0 +1,17 @@
+import {
+  Monitor,
+  User,
+  Code2,
+  BookOpenCheck,
+  FileCode,
+  Contact,
+} from "lucide-react";
+
+export const navigationLinks = [
+  { label: "Home", path: "#", icon: Monitor },
+  { label: "About", path: "#about", icon: User },
+  { label: "Skills", path: "#skills", icon: Code2 },
+  { label: "Services", path: "#services", icon: FileCode },
+  { label: "Portfolio", path: "#portfolio", icon: BookOpenCheck },
+  { label: "Contact", path: "#contact", icon: Contact },
+];
