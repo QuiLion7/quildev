1e7e504b6ccd9ee05a8e8e291cda6b64cbc4fdcf
QuiLion7 <quilbrub@gmail.com>
2023-12-26 19:32:23 -0300

    feat: add PageHome component to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import { ModeToggle } from "@/components/mode-toggle";
+import PageHome from "@/components/page-home";
 
 export default function Home() {
   return (
-    <div className="relative container flex justify-center items-center mt-1">
+    <div className=" relative container flex justify-center items-center p-0">
       <div className="absolute top-10 right-10">
         <ModeToggle />
       </div>
-      <p>Iniciando Projeto</p>
+      <PageHome />
     </div>
   );
 }
