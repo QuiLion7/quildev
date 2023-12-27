83858825607d659055e202e5b6c8f6ca56d893cd
QuiLion7 <quilbrub@gmail.com>
2023-12-27 02:15:51 -0300

    style: update responsiveness of home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
 import { ModeToggle } from "@/components/mode-toggle";
 import PageHome from "@/components/page-home";
+import PageSkills from "@/components/page-skills";
+import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
   return (
-    <div className=" relative container flex justify-center items-center p-0">
+    <div className=" relative container flex flex-col justify-center items-center p-0">
       <div className="absolute top-10 right-10">
         <ModeToggle />
       </div>
       <PageHome />
+
+      <Separator />
+
+      <PageSkills />
+
+      <Separator />
     </div>
   );
 }
