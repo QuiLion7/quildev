fe5d0d63b34fb3392960871201a5219cf45b554d
QuiLion7 <quilbrub@gmail.com>
2023-12-28 01:31:53 -0300

    chore: update data.ts with new routes

--- src/app/utils/data.ts
+++ src/app/utils/data.ts
@@ -8,10 +8,10 @@ import {
 } from "lucide-react";
 
 export const navigationLinks = [
-  { label: "Inicio", path: "#home", icon: Monitor },
-  { label: "Sobre Mim", path: "#about", icon: User },
-  { label: "Habilidades Técnicas", path: "#skills", icon: Code2 },
+  { label: "Inicio", path: "/", icon: Monitor },
+  { label: "Sobre Mim", path: "/about", icon: User },
+  { label: "Habilidades Técnicas", path: "/skills", icon: Code2 },
   // { label: "Services", path: "#services", icon: FileCode },
-  { label: "Projetos", path: "#portfolio", icon: BookOpenCheck },
-  { label: "Contato", path: "#contact", icon: Contact },
+  { label: "Projetos", path: "/portfolio", icon: BookOpenCheck },
+  { label: "Contato", path: "/contact", icon: Contact },
 ];
