ad0e7a6e5499e6d79d4f0b83d95fcdaa30dd9f6e
QuiLion7 <quilbrub@gmail.com>
2023-12-28 01:35:59 -0300

    feat: create contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import PageHome from "@/components/page-home";
+
+export default function Contact() {
+  return (
+    <div className=" relative container flex flex-col justify-center items-center p-0">
+      Contato
+    </div>
+  );
+}
