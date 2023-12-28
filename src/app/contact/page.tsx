555e9f65132a29d88962b7a66a62ad120d47c100
QuiLion7 <quilbrub@gmail.com>
2023-12-28 09:10:23 -0300

    fix: resolve deployment error in contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,6 @@
-import { ModeToggle } from "@/components/mode-toggle";
-import PageHome from "@/components/page-home";
-
 export default function Contact() {
   return (
-    <div className=" relative container flex flex-col justify-center items-center p-0">
+    <div className=" container relative flex flex-col items-center justify-center p-0">
       Contato
     </div>
   );
