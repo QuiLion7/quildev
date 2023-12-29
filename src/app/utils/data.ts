dff807a82de14dd9e75173009a66b445c3dc3ce2
QuiLion7 <quilbrub@gmail.com>
2023-12-29 01:31:44 -0300

    feat: update navigation sequence in data.ts

--- src/app/utils/data.ts
+++ src/app/utils/data.ts
@@ -9,8 +9,8 @@ import {
 
 export const navigationLinks = [
   { label: "Inicio", path: "/", icon: Monitor },
-  { label: "Sobre Mim", path: "/about", icon: User },
   { label: "Habilidades Técnicas", path: "/skills", icon: Code2 },
+  { label: "Sobre Mim", path: "/about", icon: User },
   // { label: "Services", path: "#services", icon: FileCode },
   { label: "Projetos", path: "/portfolio", icon: BookOpenCheck },
   { label: "Contato", path: "/contact", icon: Contact },
