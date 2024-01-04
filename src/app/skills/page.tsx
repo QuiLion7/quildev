e49615200e2b3539fd97de8b9deb1d448f361fe3
QuiLion7 <quilbrub@gmail.com>
2024-01-03 23:23:59 -0300

    feat: update icon sizes and displayed tags in skills page and adjust section sizes, opacity, and layout in skills page and implement responsive display of tags and sections in skills page

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Footer from "@/components/footer";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { cn } from "@/lib/utils";
@@ -12,7 +13,9 @@ export default function Skills() {
   const technicalAbilities = [
     {
       name: "Front-End",
-      iconPath: <LayoutTemplate className="h-[20px] w-[20px]" />,
+      iconPath: (
+        <LayoutTemplate className="h-[18px] w-[18px] sm:h-[20px] sm:w-[20px]" />
+      ),
       skills: [
         {
           name: "JavaScript",
@@ -74,7 +77,9 @@ export default function Skills() {
     },
     {
       name: "Back-End",
-      iconPath: <FileCog className="h-[20px] w-[20px]" />,
+      iconPath: (
+        <FileCog className="h-[18px] w-[18px] sm:h-[20px] sm:w-[20px]" />
+      ),
       skills: [
         {
           name: "Node",
@@ -106,7 +111,9 @@ export default function Skills() {
     },
     {
       name: "Banco de Dados",
-      iconPath: <Database className="h-[20px] w-[20px]" />,
+      iconPath: (
+        <Database className="h-[18px] w-[18px] sm:h-[20px] sm:w-[20px]" />
+      ),
       skills: [
         {
           name: "Firebase",
@@ -127,7 +134,9 @@ export default function Skills() {
     },
     {
       name: "Outros",
-      iconPath: <UserPlus className="h-[20px] w-[20px]" />,
+      iconPath: (
+        <UserPlus className="h-[18px] w-[18px] sm:h-[20px] sm:w-[20px]" />
+      ),
       skills: [
         {
           name: "Git",
@@ -174,81 +183,86 @@ export default function Skills() {
   };
 
   return (
-    <div className="flex min-h-[91.57vh] w-full flex-col items-center justify-center gap-2 px-[2%] py-2 text-center">
-      <h1 className="w-[95%] text-xl font-bold uppercase sm:text-2xl lg:text-3xl">
-        Tecnologias e Ferramentas
-      </h1>
-      <h2 className="mb-4 w-[80%] text-xs md:text-sm lg:text-base">
-        Explore as tecnologias e ferramentas que impulsionam a minha experiência
-        como desenvolvedor Front-End.
-      </h2>
-      <div className="flex w-full flex-col items-center justify-center gap-4  p-2 lg:flex-row">
-        <section className="grid h-auto max-h-[330px] w-full max-w-[655px] grid-cols-1 gap-4 sm:grid-cols-2 lg:h-[330px]">
-          {technicalAbilities.map((ability, index) => (
-            <Button
-              key={index}
-              variant="simple"
-              className={cn(
-                "flex h-full w-full items-center justify-center rounded-lg border-2 border-primary p-2",
-                selectedCategory === ability.name ? "bg-secondary" : "",
-              )}
-              onClick={() => handleCategoryClick(ability.name)}
-            >
-              <div className="relative flex h-full w-full uppercase">
-                <h2 className="mb-2 flex w-full items-center justify-center text-xs font-bold sm:text-lg">
-                  {ability.name}
-                </h2>
-                <div className="absolute left-[-20px] top-[-20px] rounded-lg border-2 border-primary bg-secondary p-2 sm:left-[-18px] sm:top-[-18px]">
-                  {ability.iconPath}
+    <div className="flex h-full min-h-[93.65dvh] w-full flex-col items-center justify-center bg-[url('/skills.jpg')] bg-cover bg-center bg-no-repeat text-center">
+      <div className="flex h-[93.65dvh] w-full flex-col items-center justify-center overflow-y-auto bg-background/80 px-[2%] py-4">
+        <div className="flex h-full min-h-[90%] w-full flex-col items-center justify-center gap-2 px-[2%]">
+          <h1 className=" hidden h-auto w-[95%] text-lg font-bold uppercase sm:block sm:text-2xl lg:text-3xl">
+            Tecnologias e Ferramentas
+          </h1>
+          <h2 className="hidden h-auto w-[80%] text-xs sm:block md:mb-4 md:text-sm lg:text-base">
+            Explore as tecnologias e ferramentas que impulsionam a minha
+            experiência como desenvolvedor Front-End.
+          </h2>
+          <div className="flex h-[540px] w-full flex-col items-center justify-start gap-4 p-2 sm:h-auto lg:flex-row">
+            <section className="grid h-auto max-h-[330px] w-full max-w-[655px] grid-cols-1 gap-4 sm:grid-cols-2 lg:h-[330px]">
+              {technicalAbilities.map((ability, index) => (
+                <Button
+                  key={index}
+                  variant="simple"
+                  className={cn(
+                    "flex h-full w-full items-center justify-center rounded-lg border-2 border-primary p-2",
+                    selectedCategory === ability.name ? "bg-secondary/70" : "",
+                  )}
+                  onClick={() => handleCategoryClick(ability.name)}
+                >
+                  <div className="relative flex h-full w-full items-center justify-center uppercase">
+                    <h2 className="my-1 flex w-full items-center justify-center text-xs font-bold sm:text-lg">
+                      {ability.name}
+                    </h2>
+                    <div className="absolute left-[-20px] top-[-20px] rounded-lg border-2 border-primary bg-secondary p-2 sm:left-[-18px] sm:top-[-18px]">
+                      {ability.iconPath}
+                    </div>
+                  </div>
+                </Button>
+              ))}
+            </section>
+            <section className="h-auto w-full max-w-[650px] rounded-lg border-2 border-primary duration-300 hover:bg-background/30 hover:duration-300 lg:h-[330px]">
+              {selectedSkills.map((ability, index) => (
+                <div
+                  className="flex h-full w-full flex-col items-start justify-start rounded-lg"
+                  key={index}
+                >
+                  <h2 className="text-md flex w-full items-center justify-center py-1 font-bold uppercase sm:py-2 md:text-lg">
+                    {ability.name}
+                  </h2>
+                  <Separator className="h-[2px] bg-primary" />
+                  <div className="flex h-full w-full items-center justify-center p-1">
+                    <div className="grid h-auto w-full grid-cols-4 items-center justify-center gap-[0.3rem] p-1 sm:grid-cols-3 sm:flex-row sm:gap-2 md:p-2">
+                      {ability.skills.map((skill, skillIndex) => (
+                        <Button
+                          variant="secondary"
+                          asChild
+                          key={skillIndex}
+                          className="flex w-full flex-col items-center justify-start bg-secondary/70 hover:bg-secondary/100 hover:text-primary sm:flex-row"
+                        >
+                          <Link
+                            href={skill.documentationUrl}
+                            target="_blank"
+                            className="flex h-full w-full items-center justify-center rounded-lg bg-primary sm:justify-start"
+                          >
+                            <Image
+                              src={skill.iconPath}
+                              width={0}
+                              height={0}
+                              className="h-7 w-7 rounded-lg bg-primary-foreground/40 p-1 sm:h-9 sm:w-9"
+                              sizes="100%"
+                              priority
+                              alt={skill.name}
+                            />
+                            <span className="mt-1 flex h-[22px] items-center justify-center text-wrap text-[0.6rem] font-bold uppercase leading-3 sm:ml-2 sm:mt-0 sm:text-left sm:text-xs">
+                              {skill.name}
+                            </span>
+                          </Link>
+                        </Button>
+                      ))}
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </Button>
-          ))}
-        </section>
-        <section className="h-auto max-h-[330px] w-full max-w-[650px] rounded-lg border-2 border-primary lg:h-[330px]">
-          {selectedSkills.map((ability, index) => (
-            <div
-              className="flex h-full w-full flex-col items-start justify-start rounded-lg"
-              key={index}
-            >
-              <h2 className="text-md flex w-full items-center justify-center py-2 uppercase md:text-lg">
-                {ability.name}
-              </h2>
-              <Separator className="h-[2px] bg-primary" />
-              <div className="flex h-full w-full items-center justify-center p-1">
-                <div className="grid h-auto w-full grid-cols-4 items-center justify-center gap-[0.3rem] p-1 sm:grid-cols-3 sm:flex-row sm:gap-2 md:p-2">
-                  {ability.skills.map((skill, skillIndex) => (
-                    <Button
-                      variant="secondary"
-                      asChild
-                      key={skillIndex}
-                      className="flex w-full flex-col items-center justify-start hover:bg-primary hover:text-secondary sm:flex-row"
-                    >
-                      <Link
-                        href={skill.documentationUrl}
-                        target="_blank"
-                        className="flex h-full w-full items-center justify-center rounded-lg bg-primary sm:justify-start"
-                      >
-                        <Image
-                          src={skill.iconPath}
-                          width={0}
-                          height={0}
-                          className=" h-7 w-7 rounded-lg bg-primary-foreground/40 p-1 sm:h-9 sm:w-9"
-                          sizes="100%"
-                          priority
-                          alt={skill.name}
-                        />
-                        <span className="mt-1 flex h-[22px] items-center justify-center text-wrap text-[0.6rem] uppercase leading-3 sm:ml-2 sm:mt-0 sm:text-left sm:text-xs">
-                          {skill.name}
-                        </span>
-                      </Link>
-                    </Button>
-                  ))}
-                </div>
-              </div>
-            </div>
-          ))}
-        </section>
+              ))}
+            </section>
+          </div>
+        </div>
+        <Footer />
       </div>
     </div>
   );
