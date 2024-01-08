a844ee290c7283c9f1a11820dde6877d2071c83d
QuiLion7 <quilbrub@gmail.com>
2024-01-08 00:26:53 -0300

    style: resize div in footer component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,30 +4,30 @@ import { Github, Instagram, Linkedin, Youtube } from "lucide-react";
 
 export default function Footer() {
   return (
-    <div className=" flex h-auto min-h-[10%] w-full flex-row items-center justify-center gap-4">
+    <div className=" flex h-full w-full flex-row items-center justify-center gap-4">
       <Link
-        className="rounded-lg p-1 duration-300 hover:scale-125 hover:rounded-lg hover:bg-primary hover:text-secondary hover:duration-300"
+        className="rounded-lg p-1 duration-300 hover:scale-125 hover:rounded-lg hover:bg-secondary-foreground hover:text-secondary hover:duration-300"
         href="https://github.com/QuiLion7"
         target="_blank"
       >
         <Github className="h-5 w-5 md:h-6 md:w-6" />
       </Link>
       <Link
-        className="rounded-lg p-1 duration-300 hover:scale-125 hover:rounded-lg hover:bg-primary hover:text-secondary hover:duration-300"
+        className="rounded-lg p-1 duration-300 hover:scale-125 hover:rounded-lg hover:bg-secondary-foreground hover:text-secondary hover:duration-300"
         href="https://www.linkedin.com/in/quilion7/"
         target="_blank"
       >
         <Linkedin className="h-5 w-5 md:h-6 md:w-6" />
       </Link>
       <Link
-        className="rounded-lg p-1 duration-300 hover:scale-125 hover:rounded-lg hover:bg-primary hover:text-secondary hover:duration-300"
+        className="rounded-lg p-1 duration-300 hover:scale-125 hover:rounded-lg hover:bg-secondary-foreground hover:text-secondary hover:duration-300"
         href="https://www.instagram.com/quilion7"
         target="_blank"
       >
         <Instagram className="h-5 w-5 md:h-6 md:w-6" />
       </Link>
       <Link
-        className="rounded-lg p-1 duration-300 hover:scale-125 hover:rounded-lg hover:bg-primary hover:text-secondary hover:duration-300"
+        className="rounded-lg p-1 duration-300 hover:scale-125 hover:rounded-lg hover:bg-secondary-foreground hover:text-secondary hover:duration-300"
         href="https://www.youtube.com/@quilcode"
         target="_blank"
       >
