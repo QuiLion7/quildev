1e778b50f234eee90dc33631ae9909753b309abf
QuiLion7 <quilbrub@gmail.com>
2024-06-21 01:29:59 -0300

    refactor: update RootLayout with new components and font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,18 @@
 import type { Metadata } from "next";
-import { Nunito_Sans } from "next/font/google";
+import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/app/providers/theme-provider";
 import Header from "@/components/header";
 import { cn } from "@/lib/utils";
+import PageTransition from "@/components/page-transition";
+import StairTransition from "@/components/stair-transition";
+import Footer from "@/components/footer";
 
-const nunito_sans = Nunito_Sans({ subsets: ["latin"] });
+const jetBrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  variable: "--font-jetBrainsMono",
+});
 
 export const metadata: Metadata = {
   title: "QuiL <Dev/>",
@@ -18,11 +25,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="xl:container">
+    <html lang="en">
       <body
         className={cn(
-          nunito_sans.className,
-          "flex w-full flex-col items-center justify-center",
+          jetBrainsMono.className,
+          "max-w-screen box-border flex min-h-screen flex-col items-center justify-center overflow-x-hidden bg-background",
         )}
       >
         <ThemeProvider
@@ -32,7 +39,9 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <Header />
-          {children}
+          <StairTransition />
+          <PageTransition>{children}</PageTransition>
+          <Footer />
         </ThemeProvider>
       </body>
     </html>
