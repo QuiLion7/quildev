ec1af6d05560dd7f8beb65c64500a36629b5c39d
QuiLion7 <quilbrub@gmail.com>
2024-06-21 01:35:31 -0300

    Refactor: Restructure Skills component to use Tabs and optimize UI with motion animations.

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,273 +1,159 @@
 "use client";
 
-import Footer from "@/components/footer";
-import { Button } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator";
-import { cn } from "@/lib/utils";
-import { Database, FileCog, LayoutTemplate, UserPlus } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
-import { useState } from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { motion } from "framer-motion";
+import { education, experience, mySkills, about } from "../utils/data";
 
 export default function Skills() {
-  const technicalAbilities = [
-    {
-      name: "Front-End",
-      iconPath: (
-        <LayoutTemplate className="h-[18px] w-[18px] sm:h-[20px] sm:w-[20px]" />
-      ),
-      skills: [
-        {
-          name: "JavaScript",
-          iconPath: "/skills/javascript.svg",
-          documentationUrl:
-            "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
-        },
-        {
-          name: "TypeScript",
-          iconPath: "/skills/typescript.svg",
-          documentationUrl: "https://www.typescriptlang.org/docs/",
-        },
-        {
-          name: "React",
-          iconPath: "/skills/react.svg",
-          documentationUrl: "https://reactjs.org/",
-        },
-        {
-          name: "React Router",
-          iconPath: "/skills/react-router.svg",
-          documentationUrl: "https://reactrouter.com/",
-        },
-        {
-          name: "Next",
-          iconPath: "/skills/nextjs.svg",
-          documentationUrl: "https://nextjs.org/docs",
-        },
-        {
-          name: "TailwindCSS",
-          iconPath: "/skills/tailwindcss.svg",
-          documentationUrl: "https://tailwindcss.com/docs",
-        },
-        {
-          name: "HTML",
-          iconPath: "/skills/html.svg",
-          documentationUrl: "https://developer.mozilla.org/en-US/docs/Web/HTML",
-        },
-        {
-          name: "CSS",
-          iconPath: "/skills/css.svg",
-          documentationUrl: "https://developer.mozilla.org/en-US/docs/Web/CSS",
-        },
-        {
-          name: "Bootstrap",
-          iconPath: "/skills/bootstrap.svg",
-          documentationUrl: "https://getbootstrap.com/docs",
-        },
-        {
-          name: "Framer Motion",
-          iconPath: "/skills/framer-motion.svg",
-          documentationUrl: "https://www.framer.com/api/motion/",
-        },
-        {
-          name: "Vite",
-          iconPath: "/skills/vite.svg",
-          documentationUrl: "https://vitejs.dev/guide/",
-        },
-      ],
-    },
-    {
-      name: "Back-End",
-      iconPath: (
-        <FileCog className="h-[18px] w-[18px] sm:h-[20px] sm:w-[20px]" />
-      ),
-      skills: [
-        {
-          name: "Node",
-          iconPath: "/skills/nodejs.svg",
-          documentationUrl: "https://nodejs.org/en/docs/",
-        },
-        {
-          name: "JavaScript",
-          iconPath: "/skills/javascript.svg",
-          documentationUrl:
-            "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
-        },
-        {
-          name: "TypeScript",
-          iconPath: "/skills/typescript.svg",
-          documentationUrl: "https://www.typescriptlang.org/docs/",
-        },
-        {
-          name: "Express",
-          iconPath: "/skills/express.svg",
-          documentationUrl: "https://expressjs.com/en/starter/installing.html",
-        },
-        {
-          name: "Prisma",
-          iconPath: "/skills/prisma.svg",
-          documentationUrl: "https://www.prisma.io/docs/",
-        },
-      ],
-    },
-    {
-      name: "Banco de Dados",
-      iconPath: (
-        <Database className="h-[18px] w-[18px] sm:h-[20px] sm:w-[20px]" />
-      ),
-      skills: [
-        {
-          name: "Firebase",
-          iconPath: "/skills/firebase.svg",
-          documentationUrl: "https://firebase.google.com/docs",
-        },
-        {
-          name: "Subabase",
-          iconPath: "/skills/supabase.svg",
-          documentationUrl: "https://supabase.io/docs",
-        },
-        {
-          name: "MongoDB",
-          iconPath: "/skills/mongodb.svg",
-          documentationUrl: "https://docs.mongodb.com/",
-        },
-      ],
-    },
-    {
-      name: "Outros",
-      iconPath: (
-        <UserPlus className="h-[18px] w-[18px] sm:h-[20px] sm:w-[20px]" />
-      ),
-      skills: [
-        {
-          name: "Git",
-          iconPath: "/skills/git.svg",
-          documentationUrl: "https://git-scm.com/doc",
-        },
-        {
-          name: "GitHub",
-          iconPath: "/skills/github.svg",
-          documentationUrl: "https://docs.github.com/en/github",
-        },
-        {
-          name: "Postman",
-          iconPath: "/skills/postman.svg",
-          documentationUrl: "https://learning.postman.com/docs/",
-        },
-        {
-          name: "Stripe",
-          iconPath: "/skills/stripe.svg",
-          documentationUrl: "https://stripe.com/docs",
-        },
-        {
-          name: "Figma",
-          iconPath: "/skills/figma.svg",
-          documentationUrl: "https://help.figma.com/",
-        },
-        {
-          name: "Vercel",
-          iconPath: "/skills/vercel.svg",
-          documentationUrl: "https://vercel.com/docs",
-        },
-      ],
-    },
-  ];
-
-  const [selectedCategory, setSelectedCategory] = useState("Front-End");
-
-  const selectedSkills = technicalAbilities.filter(
-    (ability) => ability.name === selectedCategory,
-  );
-
-  const handleCategoryClick = (categoryName: string) => {
-    setSelectedCategory(categoryName);
-  };
-
   return (
-    <div className="flex h-full min-h-[93.65dvh] w-full flex-col items-center justify-center bg-[url('/skills.jpg')] bg-cover bg-center bg-no-repeat text-center">
-      <div className="flex h-full min-h-[93.65dvh] w-full flex-col items-center justify-between overflow-y-auto bg-background/90">
-        <div className="flex h-auto min-h-[84.2dvh] w-full flex-col items-center justify-center gap-2 px-[2%] pt-2">
-          <h1 className=" h-auto w-full text-lg font-bold uppercase sm:text-2xl lg:text-3xl">
-            Tecnologias e Ferramentas
-          </h1>
-          <h2 className="h-auto w-full text-xs md:mb-4 md:text-sm lg:text-base">
-            Explore as tecnologias e ferramentas que impulsionam a minha
-            experiência como desenvolvedor Front-End.
-          </h2>
-          <div className="flex h-[540px] w-full flex-col items-center justify-start gap-4 p-2 sm:h-auto lg:flex-row">
-            <section className="grid h-auto max-h-[330px] w-full max-w-[655px] grid-cols-1 gap-4 sm:grid-cols-2 lg:h-[330px]">
-              {technicalAbilities.map((ability, index) => (
-                <Button
-                  key={index}
-                  variant="simple"
-                  className={cn(
-                    "flex h-full w-full items-center justify-center border-y-2 border-primary p-2 duration-300 hover:rounded-lg",
-                    selectedCategory === ability.name
-                      ? "rounded-lg bg-secondary/80"
-                      : "",
-                  )}
-                  onClick={() => handleCategoryClick(ability.name)}
-                >
-                  <div className="relative flex h-full w-full items-center justify-center uppercase">
-                    <h2 className="my-1 flex w-full items-center justify-center text-xs font-bold sm:text-lg">
-                      {ability.name}
-                    </h2>
-                    <div className="absolute left-[-16px] top-[-20px] rounded-lg border-y-2 border-primary bg-secondary p-1 sm:left-[-18px] sm:top-[-18px] sm:p-2">
-                      {ability.iconPath}
-                    </div>
-                  </div>
-                </Button>
-              ))}
-            </section>
-            <section className="h-auto w-full max-w-[650px] border-y-2 border-primary duration-300 hover:rounded-lg hover:bg-background/50 lg:h-[330px]">
-              {selectedSkills.map((ability, index) => (
-                <div
-                  className="flex h-full w-full flex-col items-start justify-start hover:rounded-lg"
-                  key={index}
-                >
-                  <h2 className="text-md flex w-full items-center justify-center py-1 font-bold uppercase sm:py-2 md:text-lg">
-                    {ability.name}
-                  </h2>
-                  <Separator className="h-[2px] rounded-lg bg-primary" />
-                  <div className="flex h-full w-full items-center justify-center">
-                    <div className="grid h-auto w-full grid-cols-4 items-center justify-center gap-[0.3rem]  p-1 sm:grid-cols-3 sm:flex-row sm:gap-2 md:p-2">
-                      {ability.skills.map((skill, skillIndex) => (
-                        <Button
-                          variant="secondary"
-                          asChild
-                          key={skillIndex}
-                          className="flex w-full flex-col items-center justify-center rounded-lg bg-primary/65 hover:bg-secondary/100 hover:text-primary sm:flex-row"
-                        >
-                          <Link
-                            href={skill.documentationUrl}
-                            target="_blank"
-                            className="flex h-full w-full items-center justify-center bg-primary hover:rounded-lg sm:justify-start"
-                          >
-                            <Image
-                              src={skill.iconPath}
-                              width={0}
-                              height={0}
-                              className="h-7 w-7 rounded-lg bg-primary-foreground/40 p-1 sm:h-9 sm:w-9"
-                              sizes="100%"
-                              priority
-                              alt={skill.name}
-                            />
-                            <span className="mt-1 flex h-[22px] items-center justify-center text-wrap text-[0.6rem] font-bold uppercase leading-3 sm:ml-2 sm:mt-0 sm:text-left sm:text-xs">
-                              {skill.name}
-                            </span>
-                          </Link>
-                        </Button>
-                      ))}
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </section>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="my-[40px] flex h-full w-full items-center justify-center py-12 xl:py-0"
+    >
+      <div className="flex w-[90vw] items-center justify-center">
+        <Tabs
+          defaultValue="experience"
+          className="flex w-full flex-col items-center justify-center p-1 xl:flex-row"
+        >
+          <TabsList className="mx-auto flex w-full max-w-[400px] flex-col gap-4 xl:mx-0">
+            <TabsTrigger value="experience">Experiência</TabsTrigger>
+            <TabsTrigger value="skills">Habilidades</TabsTrigger>
+            <TabsTrigger value="education">Formação</TabsTrigger>
+            <TabsTrigger value="about">Sobre Mim</TabsTrigger>
+          </TabsList>
+
+          <div className="z-20 h-full w-full max-w-[600px] bg-background p-1">
+            <TabsContent value="experience" className="w-full">
+              <div className="flex flex-col gap-4 text-center">
+                <h3 className="p-3 text-2xl font-bold md:text-3xl">
+                  {experience.title}
+                </h3>
+                <p className="mx-auto p-3 text-sm text-primary-foreground sm:text-base xl:mx-0">
+                  {experience.description}
+                </p>
+                <ScrollArea className="h-[360px] p-3">
+                  <ul className="grid grid-cols-1 gap-4 lg:grid-cols-2">
+                    {experience.items.map((item, index) => (
+                      <li
+                        key={index}
+                        className="flex h-[160px] flex-col items-center justify-center gap-1 rounded-xl border-y-2 bg-secondary px-10 py-6 transition-all duration-200 hover:border-primary-foreground"
+                      >
+                        <span className="text-primary">{item.duration}</span>
+                        <h3 className="min-h-[60px] max-w-[260px] text-center text-xl">
+                          {item.position}
+                        </h3>
+                        <div className="flex items-center gap-2">
+                          <span className="h-2 w-2 rounded-full bg-primary"></span>
+                          <p className="text-primary-foreground">
+                            {item.company}
+                          </p>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+            <TabsContent value="skills" className="w-full">
+              <div className="flex flex-col gap-4 p-3 text-center">
+                <h3 className="p-3 text-2xl font-bold md:text-3xl">
+                  {mySkills.title}
+                </h3>
+                <p className="mx-auto p-3 text-sm text-primary-foreground sm:text-base xl:mx-0">
+                  {mySkills.description}
+                </p>
+                <ScrollArea className="h-[360px] p-3">
+                  <ul className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4">
+                    {mySkills.skillList.map((skill, index) => (
+                      <li key={index}>
+                        <TooltipProvider delayDuration={100}>
+                          <Tooltip>
+                            <TooltipTrigger className="group flex h-[100px] w-full items-center justify-center rounded-xl border-y-2 bg-secondary transition-all duration-200 hover:border-primary-foreground">
+                              <skill.icon className="text-6xl transition-all duration-200 group-hover:text-primary" />
+                            </TooltipTrigger>
+                            <TooltipContent>
+                              <p>{skill.name}</p>
+                            </TooltipContent>
+                          </Tooltip>
+                        </TooltipProvider>
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+            <TabsContent value="education" className="w-full">
+              <div className="flex flex-col gap-4 p-3 text-center">
+                <h3 className="p-3 text-2xl font-bold md:text-3xl">
+                  {education.title}
+                </h3>
+                <p className="mx-auto p-3 text-sm text-primary-foreground sm:text-base xl:mx-0">
+                  {education.description}
+                </p>
+                <ScrollArea className="h-[360px] p-3">
+                  <ul className="grid grid-cols-1 gap-4 lg:grid-cols-2">
+                    {education.items.map((item, index) => (
+                      <li
+                        key={index}
+                        className="flex h-[160px] flex-col items-center justify-center gap-1 rounded-xl border-y-2 bg-secondary px-10 py-6 transition-all duration-200 hover:border-primary-foreground"
+                      >
+                        <span className="text-primary">{item.duration}</span>
+                        <h3 className="min-h-[60px] max-w-[260px] text-center text-xl">
+                          {item.name}
+                        </h3>
+                        <div className="flex items-center gap-2">
+                          <span className="h-2 w-2 rounded-full bg-primary"></span>
+                          <p className="text-primary-foreground">
+                            {item.degree}
+                          </p>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+            <TabsContent value="about" className="w-full text-center">
+              <div className="flex flex-col gap-4 p-3 text-center">
+                <h3 className="p-3 text-2xl font-bold md:text-3xl">
+                  {about.title}
+                </h3>
+                <p className="mx-auto p-3 text-sm text-primary-foreground sm:text-base xl:mx-0">
+                  {about.description}
+                </p>
+                <ScrollArea className="h-[360px] p-3">
+                  <ul className="grid grid-cols-1 gap-4 xl:grid-cols-2">
+                    {about.info.map((item, index) => (
+                      <li
+                        key={index}
+                        className="flex flex-col items-center justify-center gap-1 rounded-xl border-y-2 bg-secondary p-2 transition-all duration-200 hover:border-primary-foreground"
+                      >
+                        <span className="text-center text-base text-primary">
+                          {item.fieldName}
+                        </span>
+                        <span className="text-center text-lg text-primary-foreground">
+                          {item.fieldValue}
+                        </span>
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
           </div>
-        </div>
-        <div className=" flex h-auto min-h-[9.45dvh] w-full">
-          <Footer />
-        </div>
+        </Tabs>
       </div>
-    </div>
+    </motion.div>
   );
 }
