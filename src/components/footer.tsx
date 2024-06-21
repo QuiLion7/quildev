97d7ecc15e2aa5bdaf31d3b3595c9ed15a0bf1fc
QuiLion7 <quilbrub@gmail.com>
2024-06-21 01:31:58 -0300

    refactor: update Footer component with dynamic links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,38 +1,25 @@
 "use client";
+
 import Link from "next/link";
-import { Github, Instagram, Linkedin, Youtube } from "lucide-react";
+import { footerLinks } from "@/app/utils/data";
 
 export default function Footer() {
   return (
-    <div className=" flex h-full w-full flex-row items-center justify-center gap-4">
-      <Link
-        className="rounded-lg p-1 duration-300 hover:scale-125 hover:rounded-lg hover:bg-secondary-foreground hover:text-secondary hover:duration-300"
-        href="https://github.com/QuiLion7"
-        target="_blank"
-      >
-        <Github className="h-5 w-5 md:h-6 md:w-6" />
-      </Link>
-      <Link
-        className="rounded-lg p-1 duration-300 hover:scale-125 hover:rounded-lg hover:bg-secondary-foreground hover:text-secondary hover:duration-300"
-        href="https://www.linkedin.com/in/quilion7/"
-        target="_blank"
-      >
-        <Linkedin className="h-5 w-5 md:h-6 md:w-6" />
-      </Link>
-      <Link
-        className="rounded-lg p-1 duration-300 hover:scale-125 hover:rounded-lg hover:bg-secondary-foreground hover:text-secondary hover:duration-300"
-        href="https://www.instagram.com/quilion7"
-        target="_blank"
-      >
-        <Instagram className="h-5 w-5 md:h-6 md:w-6" />
-      </Link>
-      <Link
-        className="rounded-lg p-1 duration-300 hover:scale-125 hover:rounded-lg hover:bg-secondary-foreground hover:text-secondary hover:duration-300"
-        href="https://www.youtube.com/@quilcode"
-        target="_blank"
-      >
-        <Youtube className="h-5 w-5 md:h-6 md:w-6" />
-      </Link>
-    </div>
+    <footer className="flex h-[60px] w-full items-end justify-center">
+      <ul className="flex h-full w-full items-center justify-center gap-1 p-1">
+        {footerLinks.map((link, index) => (
+          <Link
+            key={index}
+            href={link.url}
+            target="_blank"
+            className="rounded-xl transition-all duration-300 hover:bg-primary hover:text-primary-foreground"
+          >
+            <li className="p-2">
+              <link.icon />
+            </li>
+          </Link>
+        ))}
+      </ul>
+    </footer>
   );
 }
